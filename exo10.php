922927dfa4585d3702c9a0c572bc90dece00e5e5
OlivaZER <olivaxv@gmail.com>
2023-06-16 09:19:04 +0200

    rectification de l'exo

--- exo10.php
+++ exo10.php
@@ -1,47 +1,38 @@
-<h1>Exo 10</h1>
-
-<p>A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui
-affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et
-1 €.</p>
-
-
-<h2>Résultat</h2>
-
 <?php
 
-$montant_à_payer = 152;
-$somme_verser = 200;
-$monnaie = $montant_à_payer - $montant_versé; 
-$compteur = 0;
+$montantAPayer = 152;
+$montantVerse = 200;
+
+$resteAPayer = $montantVerse - $montantAPayer;
+
+$billets10 = 0;
+$billets5 = 0;
+$pieces2 = 0;
+$pieces1 = 0;
 
 
-    while( $monnaie >= 10){
-        $monnaie = $monnaie - 10;
-        $compteur = $compteur + 1;
-        echo "$compteur billets de 10 euros";
-
+while ($resteAPayer > 0) {
+    if ($resteAPayer >= 10) {
+        $resteAPayer -= 10;
+        $billets10++;
+    } elseif ($resteAPayer >= 5) {
+        $resteAPayer -= 5;
+        $billets5++;
+    } elseif ($resteAPayer >= 2) {
+        $resteAPayer -= 2;
+        $pieces2++;
+    } elseif ($resteAPayer >= 1) {
+        $resteAPayer -= 1;
+        $pieces1++;
     }
+}
 
-    while ( $monnaie >= 5 ){
-        $monnaie = $monnaie - 5;
-        $compteur = $compteur + 1;
-        echo "$compteur billet de 10 euros ";
-    }
 
-    while ( $monnaie >=2 ){
-        $monnaie = $monnaie -2;
-        $compteur = $compteur +1;
-        echo "$compteur pièces de 2 euros";
-    }
-
-    while ($monnaie >=1 ){
-        $monnaie = $monnaie - 1;
-        $compteur = $compteur +1;
-        echo "$compteur pièces de 1 euro";
-    }
-
-     
-    
-
+echo "Rendu de monnaie :\n";
+echo $billets10 . " billets de 10 €\n";
+echo $billets5 . " billet de 5 €\n";
+echo $pieces2 . " pièce de 2 €\n";
+echo $pieces1 . " pièce de 1 €\n";
+?>
 
 
