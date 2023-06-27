22bf23973fb7495e2d5b30113858ccbcab2ece22
Olivazer <olivaxv@gmail.com>
2023-06-27 16:21:19 +0200

    fin

---
+++ page2_exo11.php
@@ -0,0 +1,18 @@
+<h1>Exo11</h1>
+
+<p>
+Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d’une
+chaîne de caractère représentant une date. <br>
+Exemple :<br>
+formaterDateFr("2018-02-23");<br>
+Affichage
+vendredi 23 février 2018
+
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+    $date = ("2018-02-23");
+    
