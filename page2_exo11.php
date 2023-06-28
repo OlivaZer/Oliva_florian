d3d363a88d4b7fa129295570f36c9158cac2f0dc
OlivaZER <olivaxv@gmail.com>
2023-06-28 10:32:17 +0200

    fin

--- page2_exo11.php
+++ page2_exo11.php
@@ -3,9 +3,9 @@
 <p>
 Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d’une
 chaîne de caractère représentant une date. <br>
-Exemple :<br>
+Exemple :
 formaterDateFr("2018-02-23");<br>
-Affichage
+Affichage :
 vendredi 23 février 2018
 
 </p>
@@ -13,11 +13,54 @@ vendredi 23 février 2018
 <h2>Résultat</h2>
 
 <?php
-    $dateUS = ("2018-02-23");
-    $dateFr = new DateTime ::createFromFormat ('d/m/Y', $dateUS);
-    // strftime('%d-%m-%Y',strtotime($date));
 
-    
+
+
+function formaterDateFr($dateString) {
+    // Création d'un objet DateTime à partir de la chaîne de caractères
+    $date = new DateTime($dateString);
+  
+    // Récupération des informations de la date
+    $jourSemaine = $date->format('l');
+    $jour = $date->format('d');
+    $mois = $date->format('F');
+    $annee = $date->format('Y');
+  
+    // Conversion des noms des jours et des mois en français
+    $jourSemaineFr = [
+        'Sunday' => 'dimanche',
+        'Monday' => 'lundi',
+        'Tuesday' => 'mardi',
+        'Wednesday' => 'mercredi',
+        'Thursday' => 'jeudi',
+        'Friday' => 'vendredi',
+        'Saturday' => 'samedi'
+    ];
+    $moisFr = [
+        'January' => 'janvier',
+        'February' => 'février',
+        'March' => 'mars',
+        'April' => 'avril',
+        'May' => 'mai',
+        'June' => 'juin',
+        'July' => 'juillet',
+        'August' => 'août',
+        'September' => 'septembre',
+        'October' => 'octobre',
+        'November' => 'novembre',
+        'December' => 'décembre'
+    ];
+  
+    // Construction de la date formatée en français
+    $dateFormatee = $jourSemaineFr[$jourSemaine] . ' ' . $jour . ' ' . $moisFr[$mois] . ' ' . $annee;
+  
+    // Affichage de la date formatée
+    echo $dateFormatee;
+}
+
+
+formaterDateFr("2018-02-23");
+
 
     
 // doc :https://www.develop4fun.fr/afficher-la-date-du-jour-en-francais-avec-php/
